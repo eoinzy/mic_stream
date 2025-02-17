0aa8aa1cbaf65a3fd9a35a996de695c7883f080e
Eoin Conroy <eoinconroy@gmail.com>
2025-02-17 13:22:23 +0000

    Fix compile issues

--- android/src/main/kotlin/com/code/aaron/micstream/MicStreamPlugin.kt
+++ android/src/main/kotlin/com/code/aaron/micstream/MicStreamPlugin.kt
@@ -7,7 +7,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
+//import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** MicStreamPlugin
  * In reference to flutters official sensors plugin
